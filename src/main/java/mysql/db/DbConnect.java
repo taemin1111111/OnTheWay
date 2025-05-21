febf5bb9d9419d5d9334a9a8c102b4ae82e44724
FELT0416 <serenactb@gmail.com>
2025-05-21 10:37:48 +0900

    init

---
+++ src/main/java/mysql/db/DbConnect.java
@@ -0,0 +1,97 @@
+package mysql.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnect {
+
+static final String URL="jdbc:mysql://localhost:3306/test";
+static final String MySqlDriver="com.mysql.cj.jdbc.Driver";
+
+//생성자 안에 드라이버 넣기
+   public DbConnect() {
+	// TODO Auto-generated constructor stub
+	   try {
+		Class.forName(MySqlDriver);
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		System.out.println("Mysql 드라이버 실패: "+e.getMessage());
+	}
+   
+   }
+	
+	//오라클계정연결
+	public Connection getConnection()
+	{
+		Connection conn=null;
+		
+		try {
+			conn=DriverManager.getConnection(URL, "test", "1234");
+			
+			System.out.println("Mysql 연결 성공!!!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Mysql 연결 실패!!!");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+			
+		}
+		
+		return conn;
+	}
+	
+	//close 메서드 총4개,오버로딩 메서드
+	public void dbClose(ResultSet rs,Statement stmt,Connection conn)
+	{
+		try {
+			if(rs!=null) rs.close();
+			if(stmt!=null) stmt.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void dbClose(Statement stmt,Connection conn)
+	{
+		try {
+			
+			if(stmt!=null) stmt.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void dbClose(ResultSet rs,PreparedStatement pstmt,Connection conn)
+	{
+		try {
+			if(rs!=null) rs.close();
+			if(pstmt!=null) pstmt.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void dbClose(PreparedStatement pstmt,Connection conn)
+	{
+		try {
+			
+			if(pstmt!=null) pstmt.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
